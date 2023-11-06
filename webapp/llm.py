f7a1f970106a9d506575cea3167a850f5a6f3c44
matorix <matorixmatrix@gmail.com>
2023-11-06 09:30:33 +0900

    remove unused import adn add requirememts.txt

--- webapp/llm.py
+++ webapp/llm.py
@@ -1,21 +1,8 @@
-import torch
 from chat_frame import ChatFrame, DEFAULT_PROMPT
 from llama_cpp import Llama
 
 # en_to_ja = pipeline("translation",  model="Helsinki-NLP/opus-tatoeba-en-ja")
 llm = Llama("/home/matorix/Downloads/yarn-mistral-7b-128k.Q5_K_M.gguf", n_ctx=128000)
-
-def top_p_sampling(logits, top_p=0.95):
-    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-    cumulative_probs = torch.cumsum(torch.nn.functional.softmax(sorted_logits, dim=-1), dim=-1)
-    sorted_indices_to_remove = cumulative_probs > top_p
-    sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-    sorted_indices_to_remove[..., 0] = 0
-    indices_to_remove = sorted_indices[sorted_indices_to_remove]
-    logits[indices_to_remove] = float('-inf')
-    probs = torch.nn.functional.softmax(logits, dim=-1)
-    return torch.multinomial(probs, num_samples=1)
-
 
 def generate(prompt, max_length):
     generator =  llm(
